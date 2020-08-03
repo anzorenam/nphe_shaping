77d1781f5d3bd5713529b479d9878eecd6045041
Marcos Anzorena <anzorena@geofisica.unam.mx>
2020-08-03 15:25:56 -0500

    update

--- electronics-sim/make-netlist.sh
+++ electronics-sim/make-netlist.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
+declare -a tpeaks=("75" "100" "125" "150" "175")
+declare -a degree=("3" "5")
 declare -a Cfed=("100p" "200p" "400p")
 declare -a Rfed=("3k" "6k" "12k")
 
 Cin="100p"
 Rin="100k"
 
-for i in "${Cfed[@]}"
+for k in "${tpeaks[@]}"
 do
-  for j in "${Rfed[@]}"
+  for d in "${degree[@]}"
   do
-    python netlist.py 0 "$i" "$j" "$Cin" "$Rin"
-    ngspice -o sim.log -b preamp-0.cir > tem.txt
+    for i in "${Cfed[@]}"
+    do
+      for j in "${Rfed[@]}"
+      do
+        python netlist.py 0 "$k" "$d" "$i" "$j" "$Cin" "$Rin"
+        ngspice -o sim.log -b preamp-a0.cir > tem.txt
+      done
+    done
   done
 done
 
 rm sim.log
 rm tem.txt
 
-for i in "${Cfed[@]}"
+for k in "${tpeaks[@]}"
 do
-  for j in "${Rfed[@]}"
+  for d in "${degree[@]}"
   do
-    python netlist.py 1 "$i" "$j" "$Cin" "$Rin"
-    ngspice -o sim.log -b preamp-1.cir > tem.txt
+    for i in "${Cfed[@]}"
+    do
+      for j in "${Rfed[@]}"
+      do
+        python netlist.py 1 "$k" "$d" "$i" "$j" "$Cin" "$Rin"
+        ngspice -o sim.log -b preamp-a1.cir > tem.txt
+      done
+    done
   done
 done
 
