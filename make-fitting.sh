ca6ece03e4ebcabb639cfd8d310980a6f0470331
Marcos Anzorena <anzorena@geofisica.unam.mx>
2020-08-03 00:32:19 -0500

    make TOT conversion

---
+++ make-fitting.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+declare -a scales=("tstep-0.5" "tstep-1.0" "tstep-2.0")
+declare -a amps=("0" "1")
+declare -a crange=("0" "1")
+
+main_folder="fitting"
+
+mkdir $main_folder
+mkdir $main_folder/amp_stats0
+mkdir $main_folder/amp_stats1
+
+for s in "${scales[@]}"
+do
+  mkdir $main_folder/amp_stats0/"$s"
+  mkdir $main_folder/amp_stats1/"$s"
+done
+
+for j in "${amps[@]}"
+do
+  for k in "${crange[@]}"
+  do
+    python tot-fitting.py "$j" "$k"
+  done
+done
